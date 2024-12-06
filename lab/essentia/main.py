c2d826c135cbc006fb5207109fe0efc51ed0b7d6
David Cortinas <dcortinas33@gmail.com>
2024-12-05 18:52:00 -0600

    main app

---
+++ lab/essentia/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from classifier_models import app as classifier_app
+
+main_app = FastAPI()
+
+main_app.mount("/", classifier_app)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(main_app, host="0.0.0.0", port=8001)
