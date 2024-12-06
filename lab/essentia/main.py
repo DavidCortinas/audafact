f4d7a13a17e856407b997f60f65b5f8330bddc39
David Cortinas <dcortinas33@gmail.com>
2024-12-05 18:54:31 -0600

    port to 8000

--- lab/essentia/main.py
+++ lab/essentia/main.py
@@ -8,4 +8,4 @@ main_app.mount("/", classifier_app)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(main_app, host="0.0.0.0", port=8001)
+    uvicorn.run(main_app, host="0.0.0.0", port=8000, reload=True)
